27352065e7f6c91ec8e4abe26a7983d961761176
Alfredo Osorio <alfredo2710181@gmail.com>
2023-05-18 00:55:56 +0200

    added all imports for the icons i want to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faR, fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faCode } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas);
+library.add(faR);
 
 function App() {
   return (
@@ -12,6 +17,7 @@ function App() {
         <header className="section sec1 ehader active">
         </header>
         <main>
+        <FontAwesomeIcon icon={faHome} />
           <section className="section sec2 about"></section>
           <section className="section sec3 portfolio"></section>
           <section className="section sec4 blogs"></section>
