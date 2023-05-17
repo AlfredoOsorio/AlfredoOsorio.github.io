ca60a4b161908700616da51f377e6cabc4b5ace0
Alfredo Osorio <alfredo2710181@gmail.com>
2023-05-18 01:14:21 +0200

    adding icons to the control buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faR, fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faCode } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fas);
-library.add(faR);
+import {
+  faHome,
+  faCode,
+  faUser,
+  faBriefcase,
+  faNewspaper,
+  faEnvelopeOpen,
+  faR,
+  fas,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <body className="main-content">
-        <header className="section sec1 ehader active">
-        </header>
+        <header className="section sec1 ehader active"></header>
         <main>
-        <FontAwesomeIcon icon={faHome} />
           <section className="section sec2 about"></section>
           <section className="section sec3 portfolio"></section>
           <section className="section sec4 blogs"></section>
@@ -25,13 +32,25 @@ function App() {
         </main>
 
         <div className="controls">
-          <div className="control control-1">
-            
+          <div className="control control-1 active-btn">
+            <FontAwesomeIcon icon={faHome} />
           </div>
-          <div className="control control-2"></div>
-          <div className="control control-3"></div>
-          <div className="control control-4"></div>
-          <div className="control control-5"></div>
+
+          <div className="control control-2" data-id="about">
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+
+          <div className="control control-3" data-id="portfolio">
+            <FontAwesomeIcon icon={faBriefcase} />
+          </div>
+
+          <div className="control control-4" data-id="blogs">
+            <FontAwesomeIcon icon={faNewspaper} />
+          </div>
+
+          <div className="control control-5" data-id="contact">
+            <FontAwesomeIcon icon={faEnvelopeOpen} />
+          </div>
         </div>
       </body>
     </div>
