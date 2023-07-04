9e2c1a02edc1647826c6514f5b8c90240dd48d0a
Alfredo Osorio <alfredo2710181@gmail.com>
2023-07-04 15:20:42 +0200

    Extracting main section "about" to a component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import ControlPanel from "./components/controlPanel";
+import MainSectionAbout from "./components/mainSectionAbout";
 import MainSectionHeader from "./components/mainSectionHeader"
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
       <body className="main-content">
         <MainSectionHeader />
         <main>
-          <section className="section section2 about" id="about"></section>
+          <MainSectionAbout />
+          
           <section className="section section3 portfolio" id="portfolio"></section>
           <section className="section section4 blogs" id="blogs"></section>
           <section className="section section5 contacts" id="contact"></section>
---
+++ src/components/mainSectionAbout.tsx
@@ -0,0 +1,7 @@
+function MainSectionAbout() {
+    return (
+        <section className="section section2 about" id="about"></section>
+    );
+}
+
+export default MainSectionAbout;
