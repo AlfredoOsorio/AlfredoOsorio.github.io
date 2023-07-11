fc2e7b50ca41d822f918341117ae9eb91566d4f7
Alfredo Osorio <alfredo2710181@gmail.com>
2023-07-11 03:02:35 +0200

    Extracted main section portfolio on an external component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import ControlPanel from "./components/Control panel/controlPanel";
 import MainSectionAbout from "./components/Sections/About/mainSectionAbout";
 import MainSectionHeader from "./components/Sections/Header/mainSectionHeader"
+import MainSectionPortfolio from "./components/Sections/Portfolio Section/mainSectionPortfolio";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <main>
           <MainSectionAbout />
           
-          <section className="section section3 portfolio" id="portfolio"></section>
+          <MainSectionPortfolio />
           <section className="section section4 blogs" id="blogs"></section>
           <section className="section section5 contacts" id="contact"></section>
         </main>
---
+++ src/components/Sections/Portfolio Section/mainSectionPortfolio.tsx
@@ -0,0 +1,11 @@
+function MainSectionPortfolio() {
+  return (
+    <section className="section section3 portfolio" id="portfolio">
+        <div>
+
+        </div>
+    </section>
+  );
+}
+
+export default MainSectionPortfolio;
