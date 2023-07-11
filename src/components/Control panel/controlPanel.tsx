57f76a3a2a51dac919e44d29094d0a7d6577be85
Alfredo Osorio <alfredo2710181@gmail.com>
2023-07-11 02:01:18 +0200

    removed imports not used

--- src/components/Control panel/controlPanel.tsx
+++ src/components/Control panel/controlPanel.tsx
@@ -1,18 +1,12 @@
 import ControlButtonForSection from "./controlButton";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
-  faCode,
   faUser,
   faBriefcase,
   faNewspaper,
   faEnvelopeOpen,
-  faR,
-  fas,
 } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function ControlPanel() {
   return (
