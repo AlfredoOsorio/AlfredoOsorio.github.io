91a8c9860880900c8d76986016431ab2ab692b79
Alfredo Osorio <alfredo2710181@gmail.com>
2023-07-25 19:47:55 +0200

    Extracted the contact item container.
    Some more updates to node modules
    
    created the left and right files to extract those too

---
+++ src/components/Sections/Contact/contactItem.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+interface ContactItemProps {
+    buttonIcon: IconDefinition;
+    contactItemName: string;
+    contactItemInfo: string;
+
+}
+
+function ContactItem(props: ContactItemProps) {
+  const { buttonIcon, contactItemName, contactItemInfo } = props;
+
+  return (
+    <div className="contact-item">
+      <div className="icon">
+        <FontAwesomeIcon icon={buttonIcon} />
+        {contactItemName}
+      </div>
+      <p>: {contactItemInfo}</p>
+    </div>
+  );
+}
+
+export default ContactItem;
