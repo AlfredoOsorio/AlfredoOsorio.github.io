3fcda0e23863a73ef062066c148f7589d40f56d6
Alfredo Osorio <alfredo2710181@gmail.com>
2024-07-19 00:23:34 +0200

    Update pageTransition.tsx
    change of class names for better readability

--- src/components/Control panel/pageTransition.tsx
+++ src/components/Control panel/pageTransition.tsx
@@ -4,25 +4,25 @@ function PageTransition(clickedButtonID_Number: number) {
   const clickedSectionID: string =
     ".section" + clickedButtonID_Number.toString();
 
-  let currentButton = document.querySelectorAll(".active-btn");
+  let currentButton = document.querySelectorAll(".active-button");
   let clickedButton = document.querySelectorAll(clickedButtonID);
 
-  let currentSection = document.querySelectorAll(".active");
+  let currentSection = document.querySelectorAll(".active-section");
   let clickedSection = document.querySelectorAll(clickedSectionID);
 
   currentButton[0].className = currentButton[0].className.replace(
-    " active-btn",
+    " active-button",
     ""
   );
 
-  clickedButton[0].className = clickedButton[0].className.concat(" active-btn");
+  clickedButton[0].className = clickedButton[0].className.concat(" active-button");
 
   currentSection[0].className = currentSection[0].className.replace(
-    " active",
+    " active-section",
     ""
   );
 
-  clickedSection[0].className = clickedSection[0].className.concat(" active");
+  clickedSection[0].className = clickedSection[0].className.concat(" active-section");
 }
 
 export default PageTransition;
